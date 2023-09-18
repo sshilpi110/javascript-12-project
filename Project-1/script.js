7a22123b3a83799ad03f1bf4aadce2a7c20df101
shilpi <105913922+sshilpi110@users.noreply.github.com>
2023-09-18 11:07:17 +0530

    js12

---
+++ Project-1/script.js
@@ -0,0 +1,11 @@
+
+
+const accessKey="VPuz-SDomUcpKB4bfRL6jOv6psC8OwMf8wWfOAwe3zY"
+
+const form=document.querySelector("form")
+const InputEl=document.getElementById("search-input")
+const searchResults= document.querySelector(".search-results")
+
+const showMore= document.getElementById("show-more-button")
+
+let 
